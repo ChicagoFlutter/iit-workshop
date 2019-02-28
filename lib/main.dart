02deb6d2438072d327457bfb4d30be0725786764
Jorge Coca <jorge.coca@bmwna.com>
2019-02-27 19:23:11 -0600

    Build a ListView with random names

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:random_names/random_names.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,21 +7,22 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final numberOfItems = 500;
+    final names = generateRandomNames(numberOfItems);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'IIT Rocks!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('IIT Rocks'),
+        ),
+        body: ListView.builder(
+          itemCount: numberOfItems,
+          itemBuilder: (_, index) => Text(names[index]),
+        ),
+      ),
     );
   }
 }
@@ -63,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              generateRandomNames(1).first,
             ),
             Text(
               '$_counter',
