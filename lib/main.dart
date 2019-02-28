c5863e62b7bdb0bfb86f171151d4b57596c0c910
Jorge Coca <jorge.coca@bmwna.com>
2019-02-27 20:01:28 -0600

    using navigator to add new page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
 import 'package:random_names/random_names.dart';
 
@@ -20,65 +21,47 @@ class MyApp extends StatelessWidget {
         ),
         body: ListView.builder(
           itemCount: numberOfItems,
-          itemBuilder: (_, index) => Text(names[index]),
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(names[index]),
+              onTap: () {
+                final Widget details = Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      Icon(Icons.insert_emoticon),
+                      Text(names[index])
+                    ],
+                  ),
+                );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) {
+                      return NewPage(child: details);
+                    },
+                  ),
+                );
+              },
+            );
+          },
         ),
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class NewPage extends StatelessWidget {
+  final Widget child;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {      
-      _counter++;
-    });
-  }
+  NewPage({Key key, @required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              generateRandomNames(1).first,
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      appBar: AppBar(title: Text('Details')),
+      body: Center(child: child),
     );
   }
 }
